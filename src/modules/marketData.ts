338772b46b001a36c366bee2b59a0613d32f4042
nearnshaw <nearnshaw@decentraland.org>
2020-05-05 21:58:57 -0300

    xmas & halloween stand

---
+++ src/modules/marketData.ts
@@ -0,0 +1,49 @@
+async function getMarketData(wearableName: string) {
+  let now = String(Math.floor(Date.now() / 1000))
+  const query =
+    `
+		{
+			nfts(first: 1,  orderBy: searchOrderPrice, orderDirection: asc, where:{ category: wearable, searchText: "` +
+    wearableName +
+    `", searchOrderStatus:open, searchOrderExpiresAt_gt:` +
+    now +
+    `
+		  }) {
+			  id
+			name
+			image
+			contractAddress
+			tokenId
+			wearable{ name, representationId, collection, description, category, rarity, bodyShapes }
+			searchOrderPrice
+			searchOrderStatus
+			owner{address}
+			activeOrder {
+				id
+			  }
+			}
+		}`
+
+  log('query: ', query)
+
+  // const variables = { x, y }
+  try {
+    let response = queryGraph(query)
+    log('wearable info: ', await response)
+    return response
+  } catch (error) {
+    log(`Error fetching wearable dat `, error)
+    throw error
+  }
+}
+
+async function queryGraph(query: string) {
+  const url = 'https://api.thegraph.com/subgraphs/name/decentraland/marketplace'
+  const opts = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query }),
+  }
+  const res = await fetch(url, opts)
+  return res.json()
+}
