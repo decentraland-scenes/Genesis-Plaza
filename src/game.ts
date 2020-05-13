ea5e3ad15eaa22a638ef2cffabac1ff7f663da2b
Tak-John Cheung <tak_john@hotmail.com>
2020-05-13 12:58:49 +0100

    reverted game.ts

--- src/game.ts
+++ src/game.ts
@@ -17,7 +17,7 @@ class CameraTrackSystem implements ISystem {
   }
 }
 
-//engine.addSystem(new CameraTrackSystem())
+engine.addSystem(new CameraTrackSystem())
 
 //// ADD BUILDINGS
 
@@ -29,15 +29,15 @@ setTriggerAreas()
 
 ///TELEPORTERS
 
-// placeTeleports()
+placeTeleports()
 
 /// MUSEUM
 
-// placeMuseumPieces()
+placeMuseumPieces()
 
 /// VIDEO SCEREEN
 
-// addScreen()
+addScreen()
 
 //// WEARABLES
 
@@ -52,4 +52,4 @@ addFaceUserSystem(dummyTarget)
 addRobots(dummyTarget)
 
 //// NFTS
-// addNFTs()
+addNFTs()
