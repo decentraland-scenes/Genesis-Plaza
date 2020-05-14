f712086fd5a77aa7017682c85c2a8e343bf14698
nearnshaw <nearnshaw@decentraland.org>
2020-05-14 20:17:20 -0300

    ambient sounds

--- src/game.ts
+++ src/game.ts
@@ -13,16 +13,17 @@ import {
   updateMarketData,
   CheckServer,
 } from './modules/serverHandler'
+import { AmbientSound } from './modules/ambient'
 
 //////// HACK TO LOG POSITIONS
 
-// class CameraTrackSystem implements ISystem {
-//   update() {
-//     log(Camera.instance.position)
-//   }
-// }
+class CameraTrackSystem implements ISystem {
+  update() {
+    log(Camera.instance.position)
+  }
+}
 
-// engine.addSystem(new CameraTrackSystem())
+engine.addSystem(new CameraTrackSystem())
 
 //// ADD BUILDINGS
 
@@ -68,3 +69,27 @@ engine.addSystem(new CheckServer(messageRefreshInterval))
 
 /// FETCH DATA FOR TRADE CENTER
 updateMarketData()
+
+//// AMBIENT SOUNDS
+
+let forest1 = new AmbientSound(
+  { position: new Vector3(215, 2, 183) },
+  'sounds/Forest.mp3',
+  0,
+  true
+)
+
+let forest2 = new AmbientSound(
+  { position: new Vector3(127, 2, 110) },
+  'sounds/Forest2.mp3',
+  0,
+  true
+)
+
+let thunder = new AmbientSound(
+  { position: new Vector3(43, 45, 113) },
+  'sounds/thunder.mp3',
+  0,
+  true,
+  0.2
+)
---
+++ src/modules/ambient.ts
@@ -0,0 +1,41 @@
+import utils from '../../node_modules/decentraland-ecs-utils/index'
+
+export class AmbientSound extends Entity {
+  source: AudioSource
+  clip: AudioClip
+  constructor(
+    transform: TranformConstructorArgs,
+    sound: string,
+    interval: number = 0,
+    isPlaying: boolean = false,
+    volume: number = 1
+  ) {
+    super()
+    this.clip = new AudioClip(sound)
+    this.source = new AudioSource(this.clip)
+    this.addComponent(this.source)
+    this.addComponent(new Transform(transform))
+    engine.addEntity(this)
+
+    if (interval > 1) {
+      this.source.loop = false
+      this.addComponent(
+        new utils.Interval(interval, () => {
+          this.source.playOnce()
+        })
+      )
+    } else if (isPlaying) {
+      this.source.loop = true
+      this.source.playing = true
+    }
+
+    this.source.volume = volume
+  }
+
+  public play(): void {
+    this.source.playOnce()
+  }
+  public stop(): void {
+    this.source.playing = false
+  }
+}
