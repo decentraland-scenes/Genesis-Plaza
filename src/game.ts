49c6ce6fa91630ecc4d82f9d7e67b18695c20b95
Tak-John Cheung <tak_john@hotmail.com>
2020-05-14 15:54:58 +0100

    revert game.ts

--- src/game.ts
+++ src/game.ts
@@ -29,7 +29,7 @@ setTriggerAreas()
 
 ///TELEPORTERS
 
-// placeTeleports()
+placeTeleports()
 
 /// MUSEUM
 
@@ -37,11 +37,11 @@ placeMuseumPieces()
 
 /// VIDEO SCEREEN
 
-// addScreen()
+addScreen()
 
 //// WEARABLES
 
-// addWearables()
+addWearables()
 
 //// ROBOTS
 const dummyTarget = new Entity()
@@ -52,4 +52,4 @@ addFaceUserSystem(dummyTarget)
 addRobots(dummyTarget)
 
 //// NFTS
-// addNFTs()
+addNFTs()
