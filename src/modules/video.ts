d5fe76d00df54ee3e929801dd2683bb18c56b3a8
nearnshaw <nearnshaw@decentraland.org>
2021-05-19 15:44:01 -0300

    video loop

--- src/modules/video.ts
+++ src/modules/video.ts
@@ -5,7 +5,8 @@ export class VideoScreen extends Entity {
     screenPos: TranformConstructorArgs,
     triggerPos: TranformConstructorArgs,
     triggerScale: Vector3,
-    streamURL: string
+    streamURL: string,
+    loop?: boolean
   ) {
     super()
     engine.addEntity(this)
@@ -17,6 +18,9 @@ export class VideoScreen extends Entity {
 
     this.texture = new VideoTexture(new VideoClip(streamURL))
     this.texture.playing = false
+    if (loop) {
+      this.texture.loop = true
+    }
     const mat = new Material()
     mat.albedoTexture = this.texture
     mat.specularIntensity = 0
@@ -69,8 +73,10 @@ export function addScreen() {
     },
     { position: new Vector3(270, 5, 255) },
     new Vector3(90, 90, 90),
-    'https://vod-progressive.akamaized.net/exp=1621445484~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F496%2F22%2F552481870%2F2613592517.mp4~hmac=b791389eb41990866e1b86404bdaaceb18f3de937bbf439a16cd9f27946be2f9/vimeo-prod-skyfire-std-us/01/496/22/552481870/2613592517.mp4?filename=video-loop.mp4'
+    'https://vod-progressive.akamaized.net/exp=1621451927~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F496%2F22%2F552481870%2F2613592517.mp4~hmac=4811efa31fb50ed1b92f5867f5d518b522121ec880aa78695a3fc202cc36759c/vimeo-prod-skyfire-std-us/01/496/22/552481870/2613592517.mp4?filename=video-loop.mp4',
+    true
   )
 
   screen1.getComponent(Transform).rotate(new Vector3(1, 0, 0), 10)
+  //screen1.getComponent(Material).albedoTexture.loop = true
 }
