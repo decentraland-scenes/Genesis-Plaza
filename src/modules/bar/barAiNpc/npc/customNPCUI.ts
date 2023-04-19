5281a81342ddc0d3d4da24ff8cada607e821a0b6
AnisLahouar <38791856+AnisLahouar@users.noreply.github.com>
2023-04-20 00:19:44 +0100

    refactor: update npc dialog to custom ui prompt

--- src/modules/bar/barAiNpc/npc/customNPCUI.ts
+++ src/modules/bar/barAiNpc/npc/customNPCUI.ts
@@ -13,7 +13,7 @@ import * as ui from "@dcl/ui-scene-utils";
 
 
 import { REGISTRY } from "src/registry";
-import { Dialog, DialogWindow,ButtonData } from "@dcl/npc-scene-utils";
+import { Dialog, DialogWindow, ButtonData } from "@dcl/npc-scene-utils";
 import resources, { setSection } from "src/dcl-scene-ui-workaround/resources";
 import { closeAllInteractions } from "src/modules/bar/barAiNpc/npc/npcSetup";
 import { ChatNext, ChatPart, streamedMsgs } from "src/modules/bar/barAiNpc/npc/streamedMsgs";
@@ -23,70 +23,94 @@ const canvas = ui.canvas
 
 
 
-const inputContainer = new UIContainerRect(canvas)
-/*
-inputContainer.width = "300"
-inputContainer.height = "50"
-inputContainer.hAlign = "center"
-inputContainer.vAlign = "bottom"
-inputContainer.positionY = -10 
-inputContainer.color = Color4.Blue()
-inputContainer.opacity = 1
-inputContainer.visible = false*/
-
-const askSomethingElse = new ui.CustomPrompt(ui.PromptStyles.DARKLARGE,500,200)
+const buttonsSyle = ui.ButtonStyles.RED
+const askSomethingElse = new ui.CustomPrompt(ui.PromptStyles.DARKLARGE, 500, 200)
 askSomethingElse.hide()//immeidatly hide it
 askSomethingElse.background.vAlign = 'bottom'
+askSomethingElse.closeIcon.visible = false
 
-askSomethingElse.addText("some text",0,100,Color4.White())
-askSomethingElse.addButton("button1",-100,-100,()=>{})
-askSomethingElse.addButton("Good Bye",100,-100,()=>{
+askSomethingElse.addText("Ask something else", 0, 100, Color4.White())
+askSomethingElse.addButton("Tell me about \n Decentraland", -100, 35, () => {
+  sendMessageToAi("Tell me about Decentraland")
+}, buttonsSyle).label.fontSize = 17
+askSomethingElse.addButton("What can I do here?", 100, 35, () => {
+  sendMessageToAi("what can i do in decentraland?")
+}, buttonsSyle).label.fontSize = 17
+askSomethingElse.addButton("Good Bye", -0, -20, () => {
   showInputOverlay(false)
-})
+}, buttonsSyle)
 
-export function showInputOverlay(val:boolean){
-  if(val){
+export function showInputOverlay(val: boolean) {
+  if (val) {
     askSomethingElse.show()
-  }else{
+    askSomethingElse.closeIcon.visible = false
+  } else {
     askSomethingElse.hide()
   }
 }
 
-const inputBackground = new UIImage(askSomethingElse.background,new Texture("images/DispenserAtlas.png"))
-setSection(inputBackground,resources.backgrounds.promptBackground)
+function sendMessageToAi(message: string){
+  log("sending ", message)
+  //REGISTRY.activeNPC.dialog.closeDialogWindow()
+  closeAllInteractions()
+  //utils.setTimeout(200,()=>{ 
+  const chatMessage: serverStateSpec.ChatMessage = createMessageObject(message, undefined, GAME_STATE.gameRoom)
+  sendMsgToAI(chatMessage)
+  //} 
+  textInput.value = ""
+}
+
+
+const inputContainer = new UIContainerRect(askSomethingElse.background)
+inputContainer.width = "300"
+inputContainer.height = "50"
+inputContainer.hAlign = "center"
+inputContainer.vAlign = "bottom"
+inputContainer.color = Color4.FromHexString("#FF2C55FF")
+inputContainer.opacity = 1
+inputContainer.visible = true
+
+const inputBackground = new UIImage(inputContainer, new Texture("images/DispenserAtlas.png"))
+setSection(inputBackground, resources.backgrounds.promptBackground)
 inputBackground.width = "100%"
 inputBackground.height = "100%"
 inputBackground.vAlign = "center"
 inputBackground.hAlign = "center"
 
 
-
-const textInput = new UIInputText(askSomethingElse.background)
-textInput.width = "80%"
-textInput.height = "25px"
+const textInput = new UIInputText(inputContainer)
+textInput.width = "97%"
+textInput.height = "80%"
 textInput.vAlign = "center"
 textInput.hAlign = "center"
 textInput.fontSize = 10
-textInput.placeholder = "Ask question here"
-//textInput.placeholderColor = Color4.Gray()
-textInput.positionY = "0"
+textInput.placeholder = "  Type your questions here then hit enter..."
+textInput.vTextAlign = "center"
 textInput.isPointerBlocker = true
+textInput.focusedBackground = Color4.FromHexString("#46404AFF")
 
-
-   
 textInput.onTextSubmit = new OnTextSubmit((x) => {
   log("sending ", x)
   //REGISTRY.activeNPC.dialog.closeDialogWindow()
   closeAllInteractions()
   //utils.setTimeout(200,()=>{ 
-    const chatMessage:serverStateSpec.ChatMessage = createMessageObject(x.text,undefined,GAME_STATE.gameRoom)
-    sendMsgToAI(chatMessage) 
+  const chatMessage: serverStateSpec.ChatMessage = createMessageObject(x.text, undefined, GAME_STATE.gameRoom)
+  sendMsgToAI(chatMessage)
   //} 
-  textInput.value =""  
-}) 
+  textInput.value = ""
+})
 
-const sendButton = new UIImage(askSomethingElse.background,new Texture("images/DispenserAtlas.png"))
-setSection(sendButton,resources.buttons.roundGold) 
+const sendQuestion = new UIButton(askSomethingElse.background)
+sendQuestion.text = "Ask!"
+sendQuestion.positionX = 200
+sendQuestion.positionY = 50
+sendQuestion.width = 75
+sendQuestion.height = 75
+sendQuestion.background = Color4.White()
+
+
+const sendButton = new UIImage(askSomethingElse.background, new Texture("images/DispenserAtlas.png"))
+setSection(sendButton, resources.buttons.roundGold)
 sendButton.width = "25"
 sendButton.height = "25px"
 sendButton.vAlign = "bottom"
@@ -96,6 +120,5 @@ sendButton.hAlign = "right"
 //textInput.placeholderColor = Color4.Gray()
 sendButton.positionY = "10"
 sendButton.isPointerBlocker = true
- 
-   
- 
+
+
